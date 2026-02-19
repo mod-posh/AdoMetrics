b9656f1201ae74a9ccdc583ea29d7ea345d4aaa5
Jeffrey Patton <jeffrey@patton-tech.com>
2026-02-18 20:59:42 -0600

    Added readme function

---
+++ AdoMetrics/private/Convert-ToRoundedMinutes.ps1
@@ -0,0 +1,15 @@
+function Convert-ToRoundedMinutes {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][double]$Seconds
+    )
+
+    if ($Seconds -lt 0) { $Seconds = 0 }
+
+    # Round down if remainder <= 31 seconds, otherwise round up
+    $mins = [math]::Floor($Seconds / 60)
+    $rem  = $Seconds - ($mins * 60)
+
+    if ($rem -gt 31) { $mins++ }
+    return [int]$mins
+}
